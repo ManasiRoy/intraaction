e3e65211eab851f6d3ea09d7003b96f9f9c776b4
Front End Developer <manasiry3@gmail.com>
2023-05-09 02:09:22 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `intraaction`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "6tqGuWIxPNYnRu44jf1wDAN07Lg-2J9_L-8PPxDMRUw",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass"]
+};
