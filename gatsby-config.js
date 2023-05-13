4551064b3647ebcd8e0ff91fa7d87c9969c8b36a
Front End Developer <manasiry3@gmail.com>
2023-05-13 23:08:08 +0530

    modify env commmit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `intraaction`,
